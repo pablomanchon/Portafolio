db35291b1705b58df44f464d2c8d54b57a215189
Pablo Manchon <pablomanchon97@gmail.com>
2023-09-28 16:07:48 -0300

    cambios

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -43,7 +43,7 @@ export const Header = () => {
           </Link>
         </motion.h3>
         <motion.h3 whileHover={{ scale: 1.2, y: -10, x: 5, rotate: 5 }}>
-          <Link to='contact' smooth={true} duration={500} offset={100}>
+          <Link to='contact' smooth={true} duration={500} offset={30}>
             Contactame
           </Link>
         </motion.h3>
