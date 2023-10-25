ec06479d526880aa7b6fb162ea55abab396df6d7
Pablo Manchon <pablomanchon97@gmail.com>
2023-10-25 17:36:37 -0300

    acomodo transiciones

--- src/components/Languages.jsx
+++ src/components/Languages.jsx
@@ -79,10 +79,9 @@ export const Languages = () => {
             isInview && (
               <motion.li
                 key={index}
-                initial={{ opacity: 0, y: -20 }}
+                initial={{ opacity: 0 }}
                 animate={{
                   opacity: 1,
-                  y: 0,
                 }}
                 transition={{ duration: 0.5, delay: index / 8 }}
               >
